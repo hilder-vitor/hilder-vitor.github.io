a2207a1b65a103fa3c0c7d889a3cbdc6db11ec8f
Vitor Lima <vitor_lp@yahoo.com>
2016-11-02 16:49:32 -0200

    Refactoring: menu is now in a separate file

---
+++ left_menu.js
@@ -0,0 +1,20 @@
+/**************************************************************************
+ * 		The left menu shown in every page is constructed here.
+ * 		This script writes the HTML code of such menu when included with
+ *			<script src="left_menu.js"></script>
+ **************************************************************************/
+document.write('\
+<figure>\
+			<img src="images/paris.jpg" style="width: 100%;" />\
+			</figure>\
+			<figcaption style="text-align:center; color:white; padding: 5px">\
+			<b> Hilder Vitor Lima Pereira </b> <br /> <br />\
+			I am a PhD student at <a href="http://lasca.ic.unicamp.br" target="_blank" > LASCA </a>, a laboratory of <a href="http://ic.unicamp.br" target="_blank">University of Campinas</a>, under supervision of professor <a href="https://sites.google.com/site/dfaranha/" target="_blank"> Diego Aranha</a>.\
+			</figcaption>\
+			<ul>\
+				<li id="li_1"><a href="index.html" accesskey="1" title=""><b>Homepage</b></a></li>\
+				<li id="li_2"><a href="publications.html" accesskey="2" title=""><b>Publications</b></a></li>\
+				<li id="li_3"><a href="notes.html" accesskey="3" title=""><b>Some notes</b></a></li>\
+				<li id="li_4"><a href="contact.html" accesskey="4" title=""><b>Contact me</b></a></li>\
+			</ul>\
+');
