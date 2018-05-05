f39782ed20a5d82ffcc8325c4441b5a5b8b485f9
Hilder Vitor Lima Pereira <hilder.vitor@gmail.com>
2018-05-05 14:32:52 +0200

    Page conjugation PERFEKT German

--- left_menu.js
+++ left_menu.js
@@ -15,6 +15,7 @@ document.write('\
 				<li id="li_1"><a href="index.html" accesskey="1" title=""><b>Homepage</b></a></li>\
 				<li id="li_2"><a href="publications.html" accesskey="2" title=""><b>Publications</b></a></li>\
 				<li id="li_3"><a href="notes.html" accesskey="3" title=""><b>Some notes</b></a></li>\
-				<li id="li_4"><a href="contact.html" accesskey="4" title=""><b>Contact me</b></a></li>\
+				<li id="li_4"><a href="miscellaneous.html" accesskey="4" title=""><b>Miscellaneous</b></a></li>\
+				<li id="li_5"><a href="contact.html" accesskey="5" title=""><b>Contact me</b></a></li>\
 			</ul>\
 ');
