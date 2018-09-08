790b8d51832143833c79aa6ef4e1afe838156748
Hilder Vitor Lima Pereira <hilder.vitor@gmail.com>
2018-09-08 15:00:48 +0200

    Refactoring: now pages load footer from footer.js

---
+++ footer.js
@@ -0,0 +1,13 @@
+/**************************************************************************
+ * 		The footer shown in every page is constructed here.
+ * 		This script writes the HTML code of such footer when included with
+ *			<script src="footer.js"></script>
+ **************************************************************************/
+document.write('\
+<a href="https://github.com/hilder-vitor" target="_blank" style="padding-left:5px; padding-right:10px">\
+				<img src="images/github_icon.png" width="50px"/>\
+			</a>\
+			<a href="http://crypto.stackexchange.com/users/18666/vitor" target="_blank" style="padding-left:5px; padding-right:10px">\
+				<img src="images/crypto_stackexchange_icon.png" width="50px"/>\
+			</a>');
+
