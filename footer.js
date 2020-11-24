df4d30426be9dcf49871fed07f22c0d462f4a7f7
Hilder Vitor Lima Pereira <hilder.vitor@gmail.com>
2020-11-24 12:26:40 +0100

    Added publons' icon to the footer

--- footer.js
+++ footer.js
@@ -9,5 +9,9 @@ document.write('\
 			</a>\
 			<a href="http://crypto.stackexchange.com/users/18666/vitor" target="_blank" style="padding-left:5px; padding-right:10px">\
 				<img src="images/crypto_stackexchange_icon.png" width="50px"/>\
+			</a>\
+			<a href="https://publons.com/researcher/4028422/hilder-vitor-lima-pereira/" target="_blank" style="padding-left:5px; padding-right:10px">\
+				<img src="images/publons_icon.jpg" width="80px"/>\
 			</a>');
+
 
