8597bc3aadf164505df3d41845ddfb2ec619a17d
Hilder Vitor Lima Pereira <hilder.vitor@gmail.com>
2020-12-23 16:40:38 +0100

    Added one paper peer-review

--- footer.js
+++ footer.js
@@ -7,7 +7,7 @@ document.write('\
 <a href="https://github.com/hilder-vitor" target="_blank" style="padding-left:5px; padding-right:10px">\
 				<img src="images/github_icon.png" width="50px"/>\
 			</a>\
-			<a href="http://crypto.stackexchange.com/users/18666/vitor" target="_blank" style="padding-left:5px; padding-right:10px">\
+			<a href="http://crypto.stackexchange.com/users/18666" target="_blank" style="padding-left:5px; padding-right:10px">\
 				<img src="images/crypto_stackexchange_icon.png" width="50px"/>\
 			</a>\
 			<a href="https://publons.com/researcher/4028422/hilder-vitor-lima-pereira/" target="_blank" style="padding-left:5px; padding-right:10px">\
